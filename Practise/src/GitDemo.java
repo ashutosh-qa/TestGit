162717c164003c70f65a207ea074404dddbd552f
ashutosh-qa <ashutosh.qa@gmail.com>
2019-05-08 16:35:56 -0700

    Commit Pracitce 2

---
+++ Practise/src/GitDemo.java
@@ -0,0 +1,11 @@
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("GOing to commit to Git Repository");
+		
+	}
+
+}
