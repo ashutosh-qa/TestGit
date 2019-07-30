fda6682e530a90e63ce4508eb1b48b0ce69a8b01
ashutosh <ashutosh.qa@gmail.com>
2019-07-30 10:52:00 -0700

    First Commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("Browser Automation");
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+
+}
